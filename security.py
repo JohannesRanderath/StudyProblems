7205910312967286c5484d3e5d484f7148d97a74
JohannesRanderath <joranderathsg@gmail.com>
2021-08-30 22:12:55 +0200

    Added comments and docstrings to security.py and session_handling.py

--- security.py
+++ security.py
@@ -5,7 +5,7 @@ from itsdangerous import URLSafeTimedSerializer
 from database import get_user_hash
 
 
-ts = URLSafeTimedSerializer("CS50")
+ts = URLSafeTimedSerializer("b\"\xf5;\xb7\x92\x1f'\xacvT\xf5\xe5x6\xb2\xc4\xe4\"")
 
 # Setting up passlib to hash passwords
 # Using argon2 as hashing algorithm
@@ -15,32 +15,66 @@ pwd_context = CryptContext(
 )
 
 
-def generate_password_hash(password):
+def generate_password_hash(password: str):
+    """
+    Hash password using CryptContext and argon2
+    :param password: password to encrypt
+    :return: encrypted password
+    """
     hashed_pwd = pwd_context.hash(password)
     return hashed_pwd
 
 
-def verify_password(username, password):
+def verify_password(username: str, password: str):
+    """
+    Check if given password is correct for user with given username
+    :param username: username of account to verify password
+    :param password: plain text password to verify
+    :return: False if the password is incorrect
+    :return: True if the password is correct
+    """
     correct_hash = get_user_hash(username)
     if not correct_hash or not pwd_context.verify(password, correct_hash):
         return False
     return True
 
 
-def generate_email_confirmation_link(email):
+def generate_email_confirmation_link(email: str):
+    """
+    Generate link to send in email to confirm email address
+    :param email: email to verify
+    :return: link to confirm email
+    """
     token = ts.dumps(email, salt="email-confirmation-key")
     return url_for('confirm', token=token, _external=True)
 
 
-def generate_change_email_link(old_email, new_email):
+def generate_change_email_link(old_email: str, new_email: str):
+    """
+    Generate link to send in email to confirm changed email address
+    :param old_email: email saved in database associated with account
+    :param new_email: email to verify
+    :return: link to confirm changed email
+    """
     token = ts.dumps({"old_email": old_email, "new_email": new_email}, salt="change-email-key")
     return url_for("change_email", token=token, _external=True)
 
 
-def generate_password_reset_link(username):
+def generate_password_reset_link(username: str):
+    """
+    Generate link to send in email to reset password
+    :param username: username of user to reset password
+    :return: link to reset password
+    """
     token = ts.dumps(username, salt="reset-password-key")
     return url_for("reset_password", token=token, _external=True)
 
 
-def decrypt_token(token, salt):
+def decrypt_token(token: str, salt: str):
+    """
+    Decrypt itsdangerous token
+    :param token: Token to decrypt
+    :param salt: Salt specific to functionality
+    :return: decrypted token
+    """
     return ts.loads(token, salt=salt, max_age=3600)
--- session_handling.py
+++ session_handling.py
@@ -3,8 +3,11 @@ from functools import wraps
 
 
 def is_logged_in(route_function):
-    """Lock pages with login"""
-
+    """
+    Lock pages with login
+    :param route_function: flask route function from app to be secured by login
+    :return: function that redirects to /login if not logged in and the original route function if logged in
+    """
     @wraps(route_function)
     def do_the_magic(*args, **kwargs):
         if "username" not in session.keys():
@@ -16,6 +19,11 @@ def is_logged_in(route_function):
 
 
 def current_user():
+    """
+    Get username of logged in user if one is logged in
+    :return: username of logged in user if logged in
+    :return: None if not logged in
+    """
     if "username" in session.keys():
         return session["username"]
     else:
@@ -23,6 +31,11 @@ def current_user():
 
 
 def logout_from_session():
+    """
+    Delete session and de facto log out user
+    :return: True if successful
+    :return: False if an Exception was raised
+    """
     try:
         session.clear()
         return True
@@ -31,7 +44,13 @@ def logout_from_session():
         return False
 
 
-def login_to_session(username):
+def login_to_session(username: str):
+    """
+    Add username to session and de facto log in user
+    :param username: username of user to log in
+    :return: True if successful
+    :return: False if an Exception was raised
+    """
     try:
         session["username"] = username
         return True
