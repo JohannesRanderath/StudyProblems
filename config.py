e323af581a3d3da3113d684e5368b9406132e524
JohannesRanderath <joranderathsg@gmail.com>
2021-09-02 16:31:35 +0200

    Reactivated flask-mail

--- config.py
+++ config.py
@@ -9,10 +9,10 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-    MAIL_USERNAME = ""  # TODO: Add email address
-    MAIL_PASSWORD = ""  # TODO: Add email password
 ***REMOVED***
-    MAIL_DEFAULT_SENDER = ""  # TODO: Add email address
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 
